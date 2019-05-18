d44f65ff2cd6059e5f4e29c9793ad0ef1712e47b
Sandun Isuru Niraj <sandun.isuru@gmail.com>
2019-05-18 10:44:47 +0530

    Derana news added

--- index.js
+++ index.js
@@ -13,7 +13,40 @@ app.listen(port, function () {
     console.log("Listening to Port " + port);
 });
 
-app.get('/news', (req, res) => {
+app.get('/derana/news', (req, res) => {
+
+    const url = "http://sinhala.adaderana.lk/sinhala-hot-news.php";
+    axios.get(url)
+        .then(response => {
+
+            results = [];
+            const $ = cheerio.load(response.data, { decodeEntities: false });
+
+            $('div.news-story div.story-text h2').find('a').each((i, elem) => {
+                let news = {
+                    url: "http://sinhala.adaderana.lk/" + elem.attribs.href,
+                    text: elem.children[0].data
+                }
+                results.push(news)
+            });
+
+            $('div.news-story div.story-text div.thumb-image').find('img').each((i, elem) => {
+                results[i].image = elem.attribs.src;
+            });
+
+            $('div.news-story div.story-text').find('p').each((i, elem) => {
+                results[i].body = elem.children[0].data;
+            });
+
+            res.send({ data: results });
+
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+
+app.get('/hirunews/news', (req, res) => {
 
     const url = "http://www.hirunews.lk/sinhala/local-news.php";
     axios.get(url)
