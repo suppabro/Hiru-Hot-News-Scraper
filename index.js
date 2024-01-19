aad3c0533e53115dec0d6a01818c2dde51dcf390
suppabro <130851096+suppabro@users.noreply.github.com>
2024-01-19 20:16:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
@@ -8,67 +9,64 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Default route handler for the root path
 app.get('/', (req, res) => {
     res.send('Hello, this is the root path!');
 });
 
-app.get('/derana/news', (req, res) => {
-    const url = "http://sinhala.adaderana.lk/sinhala-hot-news.php";
-    axios.get(url)
-        .then(response => {
-            results = [];
-            const $ = cheerio.load(response.data, { decodeEntities: false });
+app.get('/derana/news', async (req, res) => {
+    try {
+        const url = "http://sinhala.adaderana.lk/sinhala-hot-news.php";
+        const response = await axios.get(url);
 
-            $('div.news-story div.story-text h2').find('a').each((i, elem) => {
-                let news = {
-                    url: "http://sinhala.adaderana.lk/" + elem.attribs.href,
-                    text: elem.children[0].data
-                }
-                results.push(news)
-            });
+        const $ = cheerio.load(response.data, { decodeEntities: false });
+        const results = [];
 
-            $('div.news-story div.story-text div.thumb-image').find('img').each((i, elem) => {
-                results[i].image = elem.attribs.src;
-            });
-
-            $('div.news-story div.story-text').find('p').each((i, elem) => {
-                results[i].body = elem.children[0].data;
-            });
-
-            res.send({ data: results });
-
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).send({ error: 'Internal Server Error' });
+        $('div.news-story div.story-text h2 a').each((i, elem) => {
+            const news = {
+                url: "http://sinhala.adaderana.lk/" + elem.attribs.href,
+                text: $(elem).text()
+            };
+            results.push(news);
         });
+
+        $('div.news-story div.story-text div.thumb-image img').each((i, elem) => {
+            results[i].image = elem.attribs.src;
+        });
+
+        $('div.news-story div.story-text p').each((i, elem) => {
+            results[i].body = $(elem).text();
+        });
+
+        res.send({ data: results });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
 
-app.get('/hirunews/news', (req, res) => {
-    const url = "http://www.hirunews.lk/sinhala/local-news.php";
-    axios.get(url)
-        .then(response => {
-            results = [];
-            const $ = cheerio.load(response.data, { decodeEntities: false });
+app.get('/hirunews/news', async (req, res) => {
+    try {
+        const url = "http://www.hirunews.lk/sinhala/local-news.php";
+        const response = await axios.get(url);
 
-            $('div.rp-ltsbx div.rp-mian div.lts-cntp').find('a').each((i, elem) => {
-                let news = {
-                    url: elem.attribs.href,
-                    text: elem.children[0].data
-                }
-                results.push(news)
-            });
-            res.send({ data: results });
+        const $ = cheerio.load(response.data, { decodeEntities: false });
+        const results = [];
 
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).send({ error: 'Internal Server Error' });
+        $('div.rp-ltsbx div.rp-mian div.lts-cntp a').each((i, elem) => {
+            const news = {
+                url: elem.attribs.href,
+                text: $(elem).text()
+            };
+            results.push(news);
         });
+
+        res.send({ data: results });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
 
 app.listen(port, () => {
     console.log("Listening to Port " + port);
 });
-   
